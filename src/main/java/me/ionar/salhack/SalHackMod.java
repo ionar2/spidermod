cbfbdc992e742773e28c222c07aad9cf7924b3c2
brixalis <64821147+brixalis@users.noreply.github.com>
2020-05-05 03:55:13 +0100

    Update SalHackMod.java

--- src/main/java/me/ionar/salhack/SalHackMod.java
+++ src/main/java/me/ionar/salhack/SalHackMod.java
@@ -12,20 +12,20 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = "salhack", name = "SalHack", version = SalHackMod.VERSION)
+@Mod(modid = "brixhack", name = "BrixHack", version = SalHackMod.VERSION)
 public final class SalHackMod
 {
-    public static final String NAME = "SalHack";
-    public static final String VERSION = "2.0";
+    public static final String NAME = "BrixHack";
+    public static final String VERSION = "1.0";
 
-    public static final Logger log = LogManager.getLogger("sal");
+    public static final Logger log = LogManager.getLogger("brix");
 
     public static final EventBus EVENT_BUS = new EventManager();
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event)
     {
-        log.info("init salhack v: " + VERSION);
+        log.info("init brixhack v: " + VERSION);
 
         SalHack.Init();
 
