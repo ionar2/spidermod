d1045ea228ea3f400770e2dc3f2a27e4ab15f39b
Artamedes <ownedned123@gmail.com>
2020-05-06 05:28:43 -0400

    Implement more options into InventoryComponent, ShowXCarry, Background, and Scale

--- src/main/java/me/ionar/salhack/gui/hud/components/InventoryComponent.java
+++ src/main/java/me/ionar/salhack/gui/hud/components/InventoryComponent.java
@@ -14,6 +14,9 @@ import net.minecraft.item.ItemStack;
 public class InventoryComponent extends HudComponentItem
 {
 	public final Value<Boolean> ShowHotbar = new Value<Boolean>("ShowHotbar", new String[] {""}, "Displays the hotbar", false);
+    public final Value<Boolean> ShowXCarry = new Value<Boolean>("ShowXCarry", new String[] {""}, "Displays the crafting inventory", true);
+    public final Value<Boolean> Background = new Value<Boolean>("Background", new String[] {""}, "Displays the Background", true);
+    public final Value<Float> Scale = new Value<Float>("Scale", new String[] {""}, "Allows you to modify the scale", 1.0f, 0.0f, 10.0f, 1.0f);
 	
     public InventoryComponent()
     {
@@ -26,12 +29,12 @@ public class InventoryComponent extends HudComponentItem
     {
         super.render(p_MouseX, p_MouseY, p_PartialTicks);
 
-        SetWidth(16 * 9);
-        SetHeight(16 * 4);
 
         GlStateManager.pushMatrix();
         RenderHelper.enableGUIStandardItemLighting();
-        RenderUtil.drawRect(GetX(), GetY(), GetX() + GetWidth(), GetY() + GetHeight(), 0x75101010); // background
+        GlStateManager.scale(Scale.getValue(), Scale.getValue(), Scale.getValue());
+        if (Background.getValue())
+            RenderUtil.drawRect(GetX(), GetY(), GetX() + GetWidth(), GetY() + GetHeight(), 0x75101010); // background
         for (int i = 0; i < 27; i++)
         {
             ItemStack itemStack = mc.player.inventory.mainInventory.get(i + 9);
@@ -53,31 +56,38 @@ public class InventoryComponent extends HudComponentItem
 	        }
         }
 
-        RenderUtil.drawRect(GetX() + GetWidth(), GetY(), GetX() + GetWidth() + 32, GetY() + 32, 0x75101010); // background
-
-        for (int i = 1; i < 5; i++)
-        {
-            ItemStack itemStack = mc.player.inventoryContainer.getInventory().get(i);
-
-            int offsetX = (int) GetX();
-            int offsetY = (int) GetY();
-
-            switch (i)
+		if (ShowXCarry.getValue())
+		{
+	        if (Background.getValue())
+	            RenderUtil.drawRect(GetX() + GetWidth(), GetY(), GetX() + GetWidth() + 32, GetY() + 32, 0x75101010); // background
+    
+            for (int i = 1; i < 5; i++)
             {
-                case 1:
-                case 2:
-                    offsetX += 128 + (i * 16);
-                    break;
-                case 3:
-                case 4:
-                    offsetX += 128 + ((i - 2) * 16);
-                    offsetY += 16;
-                    break;
+                ItemStack itemStack = mc.player.inventoryContainer.getInventory().get(i);
+    
+                int offsetX = (int) GetX();
+                int offsetY = (int) GetY();
+    
+                switch (i)
+                {
+                    case 1:
+                    case 2:
+                        offsetX += 128 + (i * 16);
+                        break;
+                    case 3:
+                    case 4:
+                        offsetX += 128 + ((i - 2) * 16);
+                        offsetY += 16;
+                        break;
+                }
+    
+                mc.getRenderItem().renderItemAndEffectIntoGUI(itemStack, offsetX, offsetY);
+                mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRenderer, itemStack, offsetX, offsetY, null);
             }
-
-            mc.getRenderItem().renderItemAndEffectIntoGUI(itemStack, offsetX, offsetY);
-            mc.getRenderItem().renderItemOverlayIntoGUI(mc.fontRenderer, itemStack, offsetX, offsetY, null);
-        }
+		}
+		
+        SetWidth(16 * 9 );
+        SetHeight(16 * (ShowHotbar.getValue() ? 4 : 3));
 
         RenderHelper.disableStandardItemLighting();
         mc.getRenderItem().zLevel = 0.0F;
