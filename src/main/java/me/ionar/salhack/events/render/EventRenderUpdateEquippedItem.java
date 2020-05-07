e3d52eac3b0c3d60f3b2379581554fcb595b43ba
Artamedes <ownedned123@gmail.com>
2020-05-07 10:39:36 -0400

    Add mixin for updateEquippedItem and solve handprogress bug. Closes #42

---
+++ src/main/java/me/ionar/salhack/events/render/EventRenderUpdateEquippedItem.java
@@ -0,0 +1,11 @@
+package me.ionar.salhack.events.render;
+
+import me.ionar.salhack.events.MinecraftEvent;
+
+public class EventRenderUpdateEquippedItem extends MinecraftEvent
+{
+    public EventRenderUpdateEquippedItem()
+    {
+        super();
+    }
+}
