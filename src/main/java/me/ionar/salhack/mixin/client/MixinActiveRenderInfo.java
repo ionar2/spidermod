bb70a30bd230a3e1bd414cc987a9084a0cd866f4
Artamedes <ownedned123@gmail.com>
2020-05-10 17:11:09 -0400

    Implement NoRender mode maps

---
+++ src/main/java/me/ionar/salhack/mixin/client/MixinActiveRenderInfo.java
@@ -0,0 +1,20 @@
+package me.ionar.salhack.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import me.ionar.salhack.util.render.RenderUtil;
+import net.minecraft.client.renderer.ActiveRenderInfo;
+import net.minecraft.entity.player.EntityPlayer;
+
+@Mixin(ActiveRenderInfo.class)
+public class MixinActiveRenderInfo
+{
+    @Inject(method = "updateRenderInfo", at = @At("RETURN"))
+    private static void updateRenderInfo(CallbackInfo info)
+    {
+        RenderUtil.updateModelViewProjectionMatrix();
+    }
+}
