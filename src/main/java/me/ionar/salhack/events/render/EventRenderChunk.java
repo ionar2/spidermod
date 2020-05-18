c04b319985a8344e469f6d2bc1d61bddb95eccb1
aceyoon <64233001+aceyoon@users.noreply.github.com>
2020-05-18 18:26:09 -0400

    Removed unnecessary class specification

--- src/main/java/me/ionar/salhack/events/render/EventRenderChunk.java
+++ src/main/java/me/ionar/salhack/events/render/EventRenderChunk.java
@@ -6,8 +6,8 @@ import net.minecraft.util.math.BlockPos;
 
 public class EventRenderChunk extends MinecraftEvent
 {
-    public net.minecraft.util.math.BlockPos BlockPos;
-    public net.minecraft.client.renderer.chunk.RenderChunk RenderChunk;
+    public BlockPos BlockPos;
+    public RenderChunk RenderChunk;
     public EventRenderChunk(RenderChunk renderChunk, BlockPos blockPos)
     {
         BlockPos = blockPos;
