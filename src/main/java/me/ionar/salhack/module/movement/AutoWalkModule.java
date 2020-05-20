e57e3bf7146f6c3b1a225dea13fa46b0625ddd37
Artamedes <ownedned123@gmail.com>
2020-05-20 18:11:48 -0400

    Remove useless code in autowalk

--- src/main/java/me/ionar/salhack/module/movement/AutoWalkModule.java
+++ src/main/java/me/ionar/salhack/module/movement/AutoWalkModule.java
@@ -1,11 +1,9 @@
 package me.ionar.salhack.module.movement;
 
-import me.ionar.salhack.events.player.EventPlayerUpdate;
 import me.ionar.salhack.events.player.EventPlayerUpdateMoveState;
 import me.ionar.salhack.module.Module;
 import me.zero.alpine.fork.listener.EventHandler;
 import me.zero.alpine.fork.listener.Listener;
-import net.minecraft.util.MovementInputFromOptions;
 
 public final class AutoWalkModule extends Module
 {
@@ -20,12 +18,4 @@ public final class AutoWalkModule extends Module
     {
         mc.player.movementInput.moveForward++;
     });
-    
-    @Override
-    public void onDisable()
-    {
-        super.onDisable();
-        
-        mc.gameSettings.keyBindForward.pressed = false;
-    }
 }
