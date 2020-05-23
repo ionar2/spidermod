f4a8c2a8c52445582f58a9c2065ad9a2a48547a3
ionar2 <64785720+ionar2@users.noreply.github.com>
2020-05-23 17:01:48 -0400

    Code style changes

--- src/main/java/me/ionar/salhack/module/misc/FakePlayer.java
+++ src/main/java/me/ionar/salhack/module/misc/FakePlayer.java
@@ -13,70 +13,80 @@ import net.minecraft.client.entity.EntityOtherPlayerMP;
 
 import org.apache.commons.io.IOUtils;
 
-public class FakePlayer extends Module {
-
+public class FakePlayer extends Module
+{
     public static final Value<String> name = new Value<>("Name", new String[] {"name"}, "Name of the fake player", "jared2013");
 
-    public FakePlayer() {
-        super("FakePlayer", new String[]
-                { "Fake" }, "Summons a fake player", "NONE", 0xDADB25, ModuleType.MISC);
+    public FakePlayer()
+    {
+        super("FakePlayer", new String[] {"Fake"}, "Summons a fake player", "NONE", 0xDADB25, ModuleType.MISC);
     }
 
-    private EntityOtherPlayerMP fakePlayer;
+    private EntityOtherPlayerMP _fakePlayer;
 
     @Override
-    public void onEnable() {
+    public void onEnable()
+    {
         super.onEnable();
-        fakePlayer = null;
+        _fakePlayer = null;
 
-        if (mc.world == null) {
+        if (mc.world == null)
+        {
             this.toggle();
             return;
         }
 
-        //If getting uuid from mojang doesn't work we use another uuid
-        try{
-            fakePlayer = new EntityOtherPlayerMP(mc.world, new GameProfile(UUID.fromString(getUuid(name.getValue())), name.getValue()));
-        }catch (Exception e){
-            fakePlayer = new EntityOtherPlayerMP(mc.world, new GameProfile(UUID.fromString("70ee432d-0a96-4137-a2c0-37cc9df67f03"), name.getValue()));
+        // If getting uuid from mojang doesn't work we use another uuid
+        try
+        {
+            _fakePlayer = new EntityOtherPlayerMP(mc.world, new GameProfile(UUID.fromString(getUuid(name.getValue())), name.getValue()));
+        }
+        catch (Exception e)
+        {
+            _fakePlayer = new EntityOtherPlayerMP(mc.world, new GameProfile(UUID.fromString("70ee432d-0a96-4137-a2c0-37cc9df67f03"), name.getValue()));
             SendMessage("Failed to load uuid, setting another one.");
         }
         SendMessage(String.format("%s has been spawned.", name.getValue()));
 
-        fakePlayer.copyLocationAndAnglesFrom(mc.player);
-        fakePlayer.rotationYawHead = mc.player.rotationYawHead;
-        mc.world.addEntityToWorld(-100, fakePlayer);
-
+        _fakePlayer.copyLocationAndAnglesFrom(mc.player);
+        _fakePlayer.rotationYawHead = mc.player.rotationYawHead;
+        mc.world.addEntityToWorld(-100, _fakePlayer);
     }
 
     @Override
-    public void onDisable() {
+    public void onDisable()
+    {
         super.onDisable();
-        mc.world.removeEntityFromWorld(-100);
+        mc.world.removeEntity(_fakePlayer);
     }
 
     @Override
-    public void toggleNoSave(){
-
+    public void toggleNoSave()
+    {
     }
 
-    //Getting uuid from a name
-    public static String getUuid(String name) {
+    // Getting uuid from a name
+    public static String getUuid(String name)
+    {
         JsonParser parser = new JsonParser();
         String url = "https://api.mojang.com/users/profiles/minecraft/" + name;
-        try {
+        try
+        {
             String UUIDJson = IOUtils.toString(new URL(url), StandardCharsets.UTF_8);
             if(UUIDJson.isEmpty()) return "invalid name";
             JsonObject UUIDObject = (JsonObject) parser.parse(UUIDJson);
             return reformatUuid(UUIDObject.get("id").toString());
-        } catch (Exception e) {
+        }
+        catch (Exception e)
+        {
             e.printStackTrace();
         }
         return "error";
     }
 
-    //Reformating a short uuid type into a long uuid type
-    private static String reformatUuid(String uuid){
+    // Reformating a short uuid type into a long uuid type
+    private static String reformatUuid(String uuid)
+    {
         String longUuid = "";
 
         longUuid += uuid.substring(1, 9) + "-";
