1c979f2415f503fe954e13cf76594b353f5199f3
Alex <40795980+AlexProgrammerDE@users.noreply.github.com>
2020-05-29 17:35:09 +0200

    Update mod info pages, add ionar2 as a codeowner and add badges (#254)

--- src/main/java/com/github/lunatrius/core/reference/Reference.java
+++ src/main/java/com/github/lunatrius/core/reference/Reference.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
     public static final String MODID = "lunatriuscore";
     public static final String NAME = "LunatriusCore";
-    public static final String VERSION = "${version}";
-    public static final String FORGE = "${forgeversion}";
-    public static final String MINECRAFT = "${mcversion}";
+    public static final String VERSION = "${version_lunatriuscore}";
+    public static final String FORGE = "${forgeVersion}";
+    public static final String MINECRAFT = "${version_minecraft}";
     public static final String PROXY_SERVER = "com.github.lunatrius.core.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.core.proxy.ClientProxy";
 
--- src/main/java/com/github/lunatrius/schematica/reference/Reference.java
+++ src/main/java/com/github/lunatrius/schematica/reference/Reference.java
@@ -6,9 +6,9 @@ import org.apache.logging.log4j.Logger;
 public class Reference {
     public static final String MODID = "schematica";
     public static final String NAME = "Schematica";
-    public static final String VERSION = "${version}";
-    public static final String FORGE = "${forgeversion}";
-    public static final String MINECRAFT = "${mcversion}";
+    public static final String VERSION = "${version_schematica}";
+    public static final String FORGE = "${forgeVersion}";
+    public static final String MINECRAFT = "${version_minecraft}";
     public static final String PROXY_SERVER = "com.github.lunatrius.schematica.proxy.ServerProxy";
     public static final String PROXY_CLIENT = "com.github.lunatrius.schematica.proxy.ClientProxy";
     public static final String GUI_FACTORY = "com.github.lunatrius.schematica.client.gui.config.GuiFactory";
